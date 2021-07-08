762676beca4b15ab92359b1ecaf3233a2115c0f5
myungJin <lee._myungjin@kt.com>
2021-07-08 17:38:37 +0900

    입출현황 삭제 추가 기능 추가

--- src/main/java/com/chungchunClean/Mappers/StockMapper.java
+++ src/main/java/com/chungchunClean/Mappers/StockMapper.java
@@ -3,7 +3,6 @@ package com.chungchunClean.Mappers;
 import java.util.HashMap;
 import java.util.List;
 
-import com.chungchunClean.vo.StockCurrentVo;
 import com.chungchunClean.vo.StockVo;
 
 import org.apache.ibatis.annotations.Mapper;
@@ -23,5 +22,7 @@ public interface StockMapper  {
     public void saveStock(StockVo params);
     
  // 재고관리 - 입출관리 화면
-    public List<StockCurrentVo> getStockCurrentList(HashMap<String,Object> params);
+    public List<StockVo> getStockCurrentList(HashMap<String,Object> params);
+    public void deleteStockCurrent(StockVo params);
+    public void saveStockCurrent(StockVo params);
 }
--- src/main/java/com/chungchunClean/Service/StockService.java
+++ src/main/java/com/chungchunClean/Service/StockService.java
@@ -3,7 +3,6 @@ package com.chungchunClean.Service;
 import java.util.HashMap;
 import java.util.List;
 
-import com.chungchunClean.vo.StockCurrentVo;
 import com.chungchunClean.vo.StockVo;
 
 
@@ -21,5 +20,7 @@ public interface StockService {
     public void saveStock(List<StockVo> params);
     
     // 재고관리 - 입출관리 화면
-    public List<StockCurrentVo> getStockCurrentList(HashMap<String,Object> params);
+    public List<StockVo> getStockCurrentList(HashMap<String,Object> params);
+    public void deleteStockCurrent(List<StockVo> params);
+    public void saveStockCurrent(List<StockVo> params);
 }
