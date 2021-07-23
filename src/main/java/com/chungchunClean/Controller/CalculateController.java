2be7a4226157494ad966d23cf1b2ce0987262c40
myungJin <lee._myungjin@kt.com>
2021-07-23 17:46:42 +0900

    정산처리 커밋

--- src/main/java/com/chungchunClean/Controller/CalculateController.java
+++ src/main/java/com/chungchunClean/Controller/CalculateController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +19,7 @@ import com.chungchunClean.Service.CalculateService;
 import com.chungchunClean.Service.DailyService;
 import com.chungchunClean.vo.CalculateVo;
 import com.chungchunClean.vo.DailyVo;
+import com.chungchunClean.vo.StockVo;
 
 @Controller
 @RequestMapping("/calculate")
@@ -27,7 +29,11 @@ public class CalculateController {
 	CalculateService calculateService;
 	
     @RequestMapping(value = "/process", method = {RequestMethod.POST , RequestMethod.GET})
-    public String moveProcess() {
+    public String moveProcess(Model model) {
+    	
+    	model.addAttribute("totalCost", calculateService.getMonTodayCost());
+    	model.addAttribute("totalAddCost", calculateService.getAddTodayCost());
+    	
         return "calculate/calculate_process";
     }
     
@@ -71,5 +77,60 @@ public class CalculateController {
     	
     	return calculateService.getAddlableCost(params);
     }
+    
+    @RequestMapping(value = "/getClassifiList")
+    @ResponseBody
+    public List<CalculateVo> getClassifiList(){
+    	
+    	List<CalculateVo> calculateList = calculateService.getClassifiList();
+    	
+    	return calculateList;
+    }
+    
+    
+    @RequestMapping(value="/deleteMonItem", method = {RequestMethod.POST , RequestMethod.GET})
+    @ResponseBody
+    public void deleteMonItem(@RequestBody List<CalculateVo> params){
+    	calculateService.deleteMonItem(params);
+    }
+    
+    @RequestMapping(value="/deleteAddItem", method = {RequestMethod.POST , RequestMethod.GET})
+    @ResponseBody
+    public void deleteAddItem(@RequestBody List<CalculateVo> params){
+    	calculateService.deleteAddItem(params);
+    }
+    
+    @RequestMapping(value="/deleteClassifiItem", method = {RequestMethod.POST , RequestMethod.GET})
+    @ResponseBody
+    public void deleteClassifiItem(@RequestBody List<CalculateVo> params){
+    	calculateService.deleteClassifiItem(params);
+    }
+    
+    @RequestMapping(value="/saveMon", method = {RequestMethod.POST , RequestMethod.GET})
+    @ResponseBody
+    public void saveMon(@RequestBody List<CalculateVo> params){
+    	calculateService.saveMon(params);
+    }
+    
+    @RequestMapping(value="/saveAdd", method = {RequestMethod.POST , RequestMethod.GET})
+    @ResponseBody
+    public void saveAdd(@RequestBody List<CalculateVo> params){
+    	calculateService.saveAdd(params);
+    }
+
+    @RequestMapping(value="/saveClassifi", method = {RequestMethod.POST , RequestMethod.GET})
+    @ResponseBody
+    public void saveClassifi(@RequestBody List<CalculateVo> params){
+    	calculateService.saveClassifi(params);
+    }
+    
+    @RequestMapping(value="/dupCheckClassifi", method = {RequestMethod.POST , RequestMethod.GET})
+    @ResponseBody
+    public String dupCheckClassifi(@RequestParam HashMap<String,String> params){
+        return calculateService.dupCheckClassifi(params); 
+    }
+
+
+
 }
 
